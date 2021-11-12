8a83713a746af292aecca6a65008c873d519d55f
BrunoArdusso <brunoardu@gmail.com>
2021-11-12 14:36:03 -0300

    LWC
    ''

--- force-app/main/default/lwc/users/users.js
+++ force-app/main/default/lwc/users/users.js
@@ -1,9 +1,12 @@
+/* eslint-disable array-callback-return */
+/* eslint-disable @lwc/lwc/no-api-reassignments */
 import { api, LightningElement, wire } from "lwc";
 import checkAllocatedResources from "@salesforce/apex/AllocationFilterController.checkAllocatedResources";
 
 export default class Users extends LightningElement {
   @api role;
   @api project;
+  @api totalCoverage;
   data;
   columns = [
     { label: "First Name", fieldName: "FirstName" },
@@ -32,9 +35,20 @@ export default class Users extends LightningElement {
     }
   ];
 
+  connectedCallback() {
+    this.totalCoverage = JSON.parse(JSON.stringify(this.totalCoverage)).filter(
+      // eslint-disable-next-line consistent-return
+      (coverage) => {
+        if (coverage.role === this.role) return coverage;
+      }
+    );
+    this.totalCoverage = this.totalCoverage[0];
+    console.log("COVERAGE2", this.totalCoverage);
+  }
+
   @wire(checkAllocatedResources, { projectID: "$project", roleName: "$role" })
+  // eslint-disable-next-line no-unused-vars
   availableUsers({ error, data }) {
-    console.log(this.role, this.projectId);
     console.log(data);
     if (data) {
       console.log(data);
